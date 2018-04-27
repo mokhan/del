2048b7cf973ffb1d63236fdb03a33a2adb4ad1ac
mo <mo.khan@gmail.com>
2018-04-27 15:30:20 -0600

    fetch ENV to blow up at startup.

--- lib/del.rb
+++ lib/del.rb
@@ -16,10 +16,10 @@ module Del
     Dotenv.load(".env.local", Pathname.new(Dir.home).join(".delrc").to_s)
 
     del = Robot.new(configuration: {
-      host: ENV["DEL_HOST"],
-      jid: ENV["DEL_JID"],
-      muc_domain: ENV["DEL_MUC_DOMAIN"],
-      password: ENV["DEL_PASSWORD"],
+      host: ENV.fetch("DEL_HOST"),
+      jid: ENV.fetch("DEL_JID"),
+      muc_domain: ENV.fetch("DEL_MUC_DOMAIN"),
+      password: ENV.fetch("DEL_PASSWORD"),
     })
     del.run
   end
